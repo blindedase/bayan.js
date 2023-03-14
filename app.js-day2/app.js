f46e19dfb88ccf8ae277907265d4559bf8dc6537
nheilph <blindedase@gmail.com>
2023-03-14 17:03:57 +0800

    Add bayan.js

---
+++ app.js-day2/app.js
@@ -0,0 +1,122 @@
+// const pass = prompt("Please enter a new password");
+
+// if(pass.length >= 8){
+//     alert("Good password!");
+// } else {
+//     alert("Bad password!");
+// }
+
+
+
+
+// if (pass.length >= 8){
+//     if(pass.indexOf(" ") === -1){
+//         console.log("Valid Password.");
+//     } else {
+//         console.log("Invalid Password.");
+//     }
+// } else {
+//     console.log("Invalid Password.");
+// }
+
+
+
+
+// let username = "admin";
+// let password = "root";
+
+// const usernameInput = prompt("Enter your username.");
+// const passwordInput = prompt("Enter your password.");
+// if (username === usernameInput && password === passwordInput) {
+//     console.log("Valid Credentials");
+// } else {
+//     console.log("Invalid Credentials");
+// }
+
+
+// const passwordInput = prompt("Please enter your password");
+
+// if(passwordInput.length >= 8 && passwordInput.indexOf(" ") === - 1){
+//     console.log("Valid Password");
+// }else {
+//     console.log("Invalid Password");
+// }
+
+// const age = prompt("Please enter a age");
+
+// if(age.number <= 18){
+//     alert("You are an adult!");
+// } else {
+//     alert("You are still a minor!");
+// }
+
+// const numberInput = prompt("Please enter a number");
+
+// if(!isNaN(numberInput)){
+//     alert("Your input is a valid number");
+// }
+// else{
+//     alert("Your input is not a valid number");
+// }
+
+// let bodyTemperatureInput = prompt("Please enter your temperature")
+
+// if (bodyTemperatureInput < 36.1){
+//     alert(`Your body temperature ${bodyTemperatureInput} is Below Normal`);
+// }
+// else if (bodyTemperatureInput > 37.2){
+//     alert(`Your body temperature ${bodyTemperatureInput} is Above Normal`);
+// }
+// else{
+//     alert(`Your body temperature ${bodyTemperatureInput} is Normal`);
+// }
+
+
+// let yearInput = prompt("Please enter year");
+
+// if ((yearInput%4==0) && (yearInput%100!=0)){
+//     alert(`Year ${yearInput} is a leap year`);
+// }
+// else if(yearInput%400==0){
+//     alert(`Year ${yearInput} is a leap year`);
+// }
+// else{
+//     alert(`Year ${yearInput} is not a leap year`);
+// }
+
+
+// let equivalenceInput = prompt("Please enter grade");
+
+// if (equivalenceInput >= 96 ){
+//     alert(`The student has an Excellent grade ${equivalenceInput} with Grade Point Equivalence of 1.`);
+// }
+// else if (equivalenceInput >=94 ){
+//     alert(`The student has a Very Good grade ${equivalenceInput} with Grade Point Equivalence of 1.25`);
+// }
+// else if (equivalenceInput >=92 ){
+//     alert(`The student has a Very Good grade ${equivalenceInput} with Grade Point Equivalence of 1.50`);
+// }
+// else if (equivalenceInput >=89 ){
+//     alert(`The student has a Good grade ${equivalenceInput} with Grade Point Equivalence of 1.75`);
+// }
+// else if (equivalenceInput >=87 ){
+//     alert(`The student has a Good grade ${equivalenceInput} with Grade Point Equivalence of 2.00`);
+// }
+// else if (equivalenceInput >=84 ){
+//     alert(`The student has a Good grade ${equivalenceInput} with Grade Point Equivalence of 2.25`);
+// }
+// else if (equivalenceInput >=82 ){
+//     alert(`The student has a Fair grade ${equivalenceInput} with Grade Point Equivalence of 2.50`);
+// }
+// else if (equivalenceInput >=79 ){
+//     alert(`The student has a Fair grade ${equivalenceInput} with Grade Point Equivalence of 2.75`);
+// }
+// else if (equivalenceInput >=75 ){
+//     alert(`The student has a Passing grade ${equivalenceInput} with Grade Point Equivalence of 3.00`);
+// }
+// else if (equivalenceInput <75 ){
+//     alert(`The student has a failure grade ${equivalenceInput} with Grade Point Equivalence of 5.00 `);
+// }
+// else{
+//     alert(`The student is incomplete`);
+// }
