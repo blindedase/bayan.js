382917882d557ce45b476261264a0436d3dba7ab
nheilph <blindedase@gmail.com>
2023-03-15 16:51:29 +0800

    Add app.js-day2

--- app.js-day2/app.js
+++ app.js-day2/app.js
@@ -120,3 +120,4 @@
 // else{
 //     alert(`The student is incomplete`);
 // }
+
---
+++ day-3/app.js
@@ -0,0 +1,71 @@
+// let friend1 = "Michael";
+// let friend2 = "Yolo";
+// let friend3 = "Mark";
+
+// // array of strings
+
+
+
+
+// const classRoom = [
+//     ["JR0", "Paolo0", "Mark0"],
+//     ["JR1", "Paol1", "Mark1"],
+//     ["JR2", "Paolo2", "Mark2"],
+//     ["JR3", "Paolo3", "Null"],
+// ];
+
+// alert(classRoom[2][0]);
+
+
+// const postComments = [
+//     {
+//         userName: "admin",
+//         likes: 101,
+//         comment: "admin page"
+//     },
+//     {
+//         userName: "admin2",
+//         likes: 102,
+//         comment: "admin page2"
+//     },
+// ];
+
+// const student ={
+//     name: "Nheil",
+//     isMale: true,
+//     grades:{
+//         prelim: 50,
+//         midterm: 75,
+//         finals: 75
+//     }
+// }
+
+// const cycleArr = ["Eat", "Code", "Sleep", "Repeat"];
+
+// let secondElement = [cycleArr[1]];
+
+// alert(`${secondElement}`);
+
+
+// const cycleArr = ["Eat", "Code", "Sleep", "Repeat"];
+
+
+// const car = {
+
+//     carName: "Handa City",
+
+//     carType: "Sedan",
+
+//     transmission: "AT",
+
+//     isNew: true
+
+// };
+
+const student = {
+    studentName: 'Juan' ,
+    studentYear: '4th',
+    studentAge: 18
+}
+
+console.log(`${student.studentName} is ${student.studentAge} years old and currently a ${student.studentYear} year student.`);
