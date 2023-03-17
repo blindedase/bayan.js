20e10a255be6d53935b51219f8d067c47763bbde
blindedase <blindedase@gmail.com>
2023-03-17 14:07:16 +0800

    Add files via upload

---
+++ day-4/app.js
@@ -0,0 +1,106 @@
+// for {
+//     [initializeExpression];
+//     [condition]
+//     [incrementExpression]
+// }
+
+
+// for ( let i = 0; i<=9; i++){
+//     console.log(i);
+// }
+
+// for (let i=0; i<=20; i+=2){
+//     console.log(i);
+// }
+
+// for (let i=100; i>=0; i-=10){
+//     console.log(i);
+// }
+
+// for (let i =20; i>=0; i++){
+//     console.log("Infinite Loop", i);
+    // if (i===25) {
+    //     break;
+    // }
+// }
+
+
+// const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fridays", "Saturday"];
+
+// for (let i=0; i<= days.length -1; i++){
+//     console.log(i, days[i]);
+// }
+
+// const 
+// lower = "abcdefghijklmnopqrstuvwxyz";
+// upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// numbers = "0123456789";
+// symbols = "[]{}()*;/,._-";
+
+// all = lower + upper + numbers + symbols;
+// length = 16;
+
+
+// console.log("all");
+
+// for ( let i = 0; i<=9; i++){
+//     console.log(i);
+// }
+
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 9];
+
+// for (let i = 0; i < nums.length; i++){
+//     console.log(nums[i]);
+// }
+
+// for (let i = 1; i>=0; i++){
+//     console.log("Infinite Loop", i);
+//     if (i===11) {
+//         break;
+//     }
+// }
+
+// let num = 0;
+
+// while(num <= 10) {
+//     console.log(num);
+//     num++;
+// }
+
+// let i = 0;
+
+// while(i <= 10){
+//     console.log(i);
+//     i+=2
+// }
+
+// function greet() {
+//     console.log(`Hi, Nheil Andales!`);
+// }
+
+// greet()
+
+
+
+// function randomNum(min, max) {
+//     return Math.floor(Math.random() * (max - min) + min);
+// }
+  
+// console.log("Random Number between 1 and 500: ", randomNum(1, 500))
+
+function convertTofahrenheit(celsius) {
+    let fahrenheit;
+    return fahrenheit;
+  }
+  
+  convertTofahrenheit(30);
+
+  function convertTofahrenheit(celsius) {
+
+    let fahrenheit = celsius * 9/5 + 32
+
+    return fahrenheit;
+  }
+  
+  convertTofahrenheit(30);
