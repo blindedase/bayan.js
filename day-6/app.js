383dba643f58f39d45bfd5cc60ddecb6b80e5b40
blindedase <blindedase@gmail.com>
2023-03-20 16:58:38 +0800

    Add day-6

---
+++ day-6/app.js
@@ -0,0 +1,137 @@
+// const fullName =[
+//     {
+//         first: "Daniel",
+//         last: "Park",
+
+//         first: "Arthur",
+//         last: "Leywin",
+
+//         first: "Johan",
+//         last: "Liebert",
+
+//         first: "Patrick",
+//         last: "Bateman",
+//     }
+
+// ]
+
+// const trimmedNames = fullName.map(function(name){
+//     return name.fullName();
+
+// })
+
+
+// activity test
+
+// const fullNames = [
+//     {
+//         first: "Daniel",
+//         last: "Andales",
+//     },
+//     {
+
+//         first: "Arthur",
+//         last: "Andales",
+//     },
+//     {
+//         first: "Johan",
+//         last: "Andales",
+//     },
+//     {
+//         first: "Patrick",
+//         last: "Andales",
+//     },
+//     {
+//         first: "Sekaku",
+//         last: "Andales",
+//     }
+// ];
+
+
+
+// const firstNames = fullNames.map(function(fullName){
+//     return fullName.first;
+// })
+
+
+
+// const animes = [
+//     {
+//         title: "Naruto",
+//         score: 100
+//     },
+//     {
+//         title: "Narutox",
+//         score: 100
+//     },
+//     {
+//         title: "Narutoxx",
+//         score: 100
+//     },
+//     {
+//         title: "Narutoxxx",
+//         score: 100
+//     },
+//     {
+//         title: "Narutozzzz",
+//         score: 100
+//     },
+// ]
+
+// const titles = animes.map(function(anime){
+//     return anime.title;
+// })
+
+// const dice = () => {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+
+// const string = (str) => str;
+
+// const add = (a, b) => a+b;
+
+
+// day-6-act-attendance
+// const Greeting = (greeter) => (
+//      console.log(`Hello, ${greeter}!`)
+// )
+
+// let ar = [98, 99, 75, 80];
+// const arrAverage = (arr) => {
+//     let total = 0;
+//     for(let number of arr){
+//       total += number;
+//     }
+//     return total/arr.length;
+//   }
+   
+//   console.log(arrAverage(ar));
+
+
+// const Persons = ["Person1", "Person2", "Person3"];
+
+// Persons.forEach(Persons => console.log(Persons))
+
+
+// let arr = ['element1','element2','element3']
+
+// let copyArr = []
+
+// arr.forEach((value, index) => copyArr[index] = value);
+
+// console.log(copyArr);
+
+// const nums = [6,7,8,9];
+
+// const sqrt = nums.map(function(num) {
+//     return Math.pow(num, 2);
+// })
+// console.log(sqrt);
+
+// const nums = [2,5,100];
+
+// const doubles = nums.map(function(num) {
+//     return num * 2;
+// })
+
+// console.log(doubles);
