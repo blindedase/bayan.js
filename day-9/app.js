f5c508a4bd1f88aaaa6782c87a694a86b9e04ee7
blindedase <blindedase@gmail.com>
2023-03-23 16:58:01 +0800

    Add day-9

---
+++ day-9/app.js
@@ -0,0 +1,36 @@
+
+// const randomColorGenerator = () => {
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+//     return `rgb(${r},${g},${b})`;
+    
+    
+// }
+
+// const btn = document.querySelector('#btnv2');
+// const h1 = document.getElementById("heading");
+
+// btn.addEventListener("click", function(){
+//     document.body.style.backgroundColor = randomColorGenerator();
+//     h1.innerHTML = document.body.style.backgroundColor;
+// })
+
+
+
+
+
+const form = document.querySelector("#testForm");
+const input = document.querySelector("input");
+const list = document.querySelector("#list");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const userInput1 = textInput1.value;
+    const userInput2 = textInput2.value;
+    const newLI = document.createElement("li");
+    newLI.innerText = `${userInput1} - ${userInput2}`;
+    list.append(newLI);
+    textInput1.value = "";
+    textInput2.value = "";
+})
